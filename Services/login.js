c7ddc5ff9ac22e52b0b73c51f1501ab3a1a514cd
DracoGilga <cesar96707@gmail.com>
2024-11-28 19:14:27 -0600

    Edit: login.html and login.js

--- Services/login.js
+++ Services/login.js
@@ -10,14 +10,14 @@ document.getElementById('loginForm').addEventListener('submit', async function (
     }
 
     try {
-        const response = await fetch('http://192.168.137.1:8080/login', {
+        const response = await fetch('http://localhost:8080/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json', // O prueba 'application/x-www-form-urlencoded'
             },
             body: JSON.stringify({
-                username: usuario, // Cambia al nombre esperado por el servidor
-                password: contrasena
+                usuario: usuario, // Cambia al nombre esperado por el servidor
+                contrasena: contrasena
             })
         });
 
