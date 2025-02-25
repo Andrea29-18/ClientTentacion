5876ddaaaaed02f1404587c597129c3db9995d59
Andrea29-18 <aavp0316@gmail.com>
2025-02-25 11:27:22 -0600

    feat: Creation Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import './Navbar.css'; // Importamos los estilos
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <span className="logo-text">D. Tentación</span>
+      </div>
+      <ul className="navbar-links">
+        <li><a href="#nosotros">Nosotros</a></li>
+        <li><a href="#sucursales">Sucursales</a></li>
+        <li><a href="#productos">Productos</a></li>
+        <li><a href="#familia">Familia</a></li>
+        <li><a href="#ordena-aqui">Ordena Aquí</a></li>
+        <li><a href="#contacto">Contacto</a></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
