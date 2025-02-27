d497e0d3ae5b588e67c93512e7f499eb439f05dd
Andrea29-18 <aavp0316@gmail.com>
2025-02-27 12:32:11 -0600

    FIX: Error en User

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,13 @@
 import './Navbar.css';
+import cartIcon from "../assets/Navbar/cartIcon.png";
+import storeIcon from "../assets/Navbar/storeIcon.png";
+import userIcon from "../assets/Navbar/userIcon.png";
 
 const Navbar = () => {
+  const handleCartClick = () => {
+    alert('Carrito de compras clickeado'); // Función para manejar el clic
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-logo">
@@ -11,7 +18,21 @@ const Navbar = () => {
         <li><a href="#sucursales">Sucursales</a></li>
         <li><a href="#productos">Productos</a></li>
         <li><a href="#familia">Familia</a></li>
-        <li><a href="#ordena-aqui">Ordena Aquí</a></li>
+        <li>
+          <button onClick={handleCartClick} className="icon-button">
+            <img src={storeIcon} alt="Sucursal" className="icon-image" /> {/* Imagen como botón */}
+          </button>
+        </li>
+        <li>
+          <button onClick={handleCartClick} className="icon-button">
+            <img src={cartIcon} alt="Carrito" className="icon-image" /> {/* Imagen como botón */}
+          </button>
+        </li>
+        <li>
+          <button onClick={handleCartClick} className="icon-button">
+            <img src={userIcon} alt="Usuario" className="icon-image" /> {/* Imagen como botón */}
+          </button>
+        </li>
       </ul>
     </nav>
   );
